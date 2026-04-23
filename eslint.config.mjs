a9edbbbe23d8033e3c5295029089a54a6a589217
shahid003 <156988842+shahid003@users.noreply.github.com>
2026-04-23 06:09:00 +0000

    homework

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,14 @@
 import js from "@eslint/js";
 import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
-]);
+export default [
+  {
+    files: ["*/.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: globals.node
+    }
+  },
+  js.configs.recommended
+];
